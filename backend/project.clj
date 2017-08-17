6bcdc19eb8f7981b9d0b2e5707cb800b9c1fd0e9
Matti Ahinko <matti.ahinko@gofore.com>
2017-08-17 23:38:41 +0300

    Add web server dependencies

--- backend/project.clj
+++ backend/project.clj
@@ -3,7 +3,13 @@
   :url "https://github.com/stormaaja/curled-earth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.6.0"]
+                 [ring/ring-defaults "0.3.1"]
+                 [ring "1.6.2"]
+                 [ring.middleware.logger "0.5.0"]
+                 [http-kit "2.2.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot curled-earth-backend.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
